141fc32c7cc44216e28a3aaed731c4828f86dffd
n3e0t8 <66070308@kmitl.ac.th>
2025-10-02 15:51:56 +0700

    add models

--- Karaokie/bookings/models.py
+++ Karaokie/bookings/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    class Role(models.TextChoices):
+        Manager = "manager"
+        Customer = "customer"
+
+    username = models.CharField(max_length=100, unique=True, null=False)
+    password = models.CharField(max_length=100, null=False)
+    email = models.EmailField(max_length=100, unique=True, null=False)
+    role = models.CharField(max_length=20, choices=Role.choices)
+
+class Customer_profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100, null=False)
+    phone = models.CharField(max_length=20, null=False)
+
+class Room_type(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    capacity = models.IntegerField(null=False)
+    price_per_hour = models.IntegerField(null=False)
+    description = models.CharField(max_length=200, null=True)
+
+class Rooms(models.Model):
+    class Status(models.TextChoices):
+        Reserved = "จองแล้ว"
+        InUse = "กำลังใช้งาน"
+        Empty = "ห้องว่าง"
+
+    name = models.CharField(max_length=50, unique=True, null=False)
+    room_type = models.ForeignKey(Room_type, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=Status.choices)
+
+class Booking(models.Model):
+    class Booking_status(models.TextChoices):
+        Pending = "รอดำเนินการ"
+        Confirmed = "ยืนยันการจอง"
+        Rejected = "ปฏิเสธ"
+        Cancel = "ยกเลิก"
+        Check_In = "เช็กอิน"
+        Check_Out = "เช็กเอาท์"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room_type, on_delete=models.CASCADE)
+    number_of_customer = models.IntegerField(null=False)
+    booking_date = models.DateField(null=False)
+    start_time = models.TimeField(null=False)
+    end_time = models.TimeField(null=False)
+    booking_status = models.CharField(max_length=20, choices=Booking_status.choices)
+    notes = models.CharField(max_length=200, null=True)
+
+class Services(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False)
+    price = models.IntegerField(null=False)
+
+class BookingServices(models.Model):
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    service = models.ForeignKey(Services, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+class Payments(models.Model):
+    class Pay_status(models.TextChoices):
+        Pending = "รอดำเนินการ"
+        Paid = "ชำระเงินแล้ว"
+
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    payment_slip = models.BinaryField(null=False)
+    payment_status = models.CharField(max_length=20, choices=Pay_status.choices)
+    pay_date = models.DateTimeField(null=False)
+    pay_create_date = models.DateTimeField(auto_now_add=True)
