e787afacaf0563f89720dfddb211118aaa3f20da
n3e0t8 <66070308@kmitl.ac.th>
2025-10-03 18:04:08 +0700

    create login template

--- Karaokie/bookings/views.py
+++ Karaokie/bookings/views.py
@@ -5,3 +5,7 @@ from django import views
 class Register(views.View):
     def get(self, request):
         return render(request, 'base.html')
+    
+def Login(request):
+    if request.method == "GET":
+        return render(request, 'Auth/Login.html')
