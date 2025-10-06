87ca2ceeaf334f53bf68de3477a6f0af6835de1f
Aiya-m <aiyarat.mkrut@gmail.com>
2025-10-06 10:01:56 +0700

    add register form

---
+++ Karaokie/bookings/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from bookings.models import Room_type, Rooms, Booking, Services, BookingServices, Payments
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterModelForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2"
+        ]
+    
