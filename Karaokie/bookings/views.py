cbeaff579cace52725843568923ca7a189d390ae
Aiya-m <aiyarat.mkrut@gmail.com>
2025-10-06 10:02:43 +0700

    edit RegisterView

--- Karaokie/bookings/views.py
+++ Karaokie/bookings/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, redirect
 from django import views
+from bookings.forms import RegisterModelForm
 
 # Create your views here.
-class Register(views.View):
+class RegisterView(views.View):
     def get(self, request):
-        return render(request, 'base.html')
+        form = RegisterModelForm()
+        return render(request, 'Auth/RegisterPage.html', {'registerform': form})
+    
+    def post(self, request):
+        form = RegisterModelForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        return render(request, 'Auth/RegisterPage.html', {'registerform': form})
     
 def Login(request):
     if request.method == "GET":
