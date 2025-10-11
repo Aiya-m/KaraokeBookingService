6781f0cdc99d5f49ed4b67022f9a6dc9e329d710
Aiya-m <aiyarat.mkrut@gmail.com>
2025-10-12 01:51:15 +0700

    edit urls

--- Karaokie/bookings/urls.py
+++ Karaokie/bookings/urls.py
@@ -3,8 +3,8 @@ from bookings.views import RegisterView, LoginView, CustomerHome, BookingList
 from . import views
 
 urlpatterns = [
-    path('/', CustomerHome.as_view(), name='customer-home'),
-    path("/register/", RegisterView.as_view(), name='register'),
-    path("/login/", LoginView.as_view(), name='login'),
-    path("/manager/bookinglist/", BookingList.as_view(), name='bookinglist')
+    path('', CustomerHome.as_view(), name='customer-home'),
+    path("register/", RegisterView.as_view(), name='register'),
+    path("login/", LoginView.as_view(), name='login'),
+    path("manager/bookinglist/", BookingList.as_view(), name='bookinglist')
 ]
