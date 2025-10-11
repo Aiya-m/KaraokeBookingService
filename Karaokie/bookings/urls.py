febb0b01b3edd24e9973ad9dfa5cf1801e9c496f
Aiya-m <aiyarat.mkrut@gmail.com>
2025-10-11 23:22:18 +0700

    add home path

--- Karaokie/bookings/urls.py
+++ Karaokie/bookings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from bookings.views import RegisterView
+from bookings.views import RegisterView, CustomerHome
 from . import views
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name='register'),
-    path("login/", views.Login, name='login')
+    path("login/", views.Login, name='login'),
+    path("customer/home/", CustomerHome.as_view(), name='customer-home')
 ]
