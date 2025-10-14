2d1bd4a8cdeb2ca0d0aab747cb9d7ee1ad1e2db7
Aiya-m <aiyarat.mkrut@gmail.com>
2025-10-14 23:19:28 +0700

    add form customer

--- Karaokie/bookings/forms.py
+++ Karaokie/bookings/forms.py
@@ -31,6 +31,64 @@ class LoginForm(forms.ModelForm):
             "password" : forms.PasswordInput(attrs={"placeholder": "รหัสผ่าน", "class": "form-control"})
         }
 
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = [
+            "number_of_customer",
+            "booking_date",
+            "start_time",
+            "end_time",
+            "phone",
+            "notes"
+        ]
+
+        widgets = {
+                "number_of_customer": forms.NumberInput(attrs={
+                    "class": "form-control",
+                    "min": 1,
+                    "placeholder": "10"
+                }),
+                "booking_date": forms.DateInput(attrs={
+                    "type": "date",
+                    "class": "form-control",
+                    "placeholder": "เลือกวันที่"
+                }),
+                "start_time": forms.TimeInput(attrs={
+                    "type": "time",
+                    "class": "form-control"
+                }),
+                "end_time": forms.TimeInput(attrs={
+                    "type": "time",
+                    "class": "form-control"
+                }),
+                "phone": forms.TextInput(attrs={
+                    "class": "form-control",
+                    "placeholder": "เช่น 0812345678"
+                }),
+            }
+        
+    notes = forms.CharField(required=False, widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "rows": 2,
+        "placeholder": "หมายเหตุ (ถ้ามี)"
+    }))
+
+class RoomsForm(forms.Form):
+    room = forms.ModelChoiceField(
+        queryset=Rooms.objects.all(),
+        empty_label="เลือกห้อง",
+        widget=forms.Select(attrs={"class": "form-control"})
+    )
+
+class ServicesForm(forms.Form):
+    services = forms.ModelChoiceField(
+        queryset=Services.objects.all(),
+        empty_label="เลือกบริการเพิ่มเติม",
+        widget=forms.Select(attrs={"class": "form-control"}),
+        required=False
+    )
+
 class ManageRoomForm(forms.ModelForm):
     class Meta:
         model = Rooms
