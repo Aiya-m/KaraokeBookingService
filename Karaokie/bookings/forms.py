35a72a2bbddb6a0a21c8fac87cc9d5c95dd9e722
Aiya-m <aiyarat.mkrut@gmail.com>
2025-10-15 03:19:49 +0700

    create customer-profile

--- Karaokie/bookings/forms.py
+++ Karaokie/bookings/forms.py
@@ -116,6 +116,17 @@ class PaymentsForm(forms.ModelForm):
             }),
         }
 
+class CustomerUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
 class ManageRoomForm(forms.ModelForm):
     class Meta:
         model = Rooms
