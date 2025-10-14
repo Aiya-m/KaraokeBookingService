7e4ff41131be8bc078c25f92f935fbd8590b6ea7
Aiya-m <aiyarat.mkrut@gmail.com>
2025-10-15 02:20:18 +0700

    add customer history and del history

--- Karaokie/bookings/urls.py
+++ Karaokie/bookings/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from bookings.views import RegisterView, LoginView, CustomerHome, BookingList, CustomerBooking, LogoutView, ManageRoom, EditRoom, CheckInNOut
+from bookings.views import RegisterView, LoginView, CustomerHome, BookingList, CustomerBooking, LogoutView, ManageRoom, EditRoom, CheckInNOut, CustomerHistory
 
 urlpatterns = [
     path('', CustomerHome.as_view(), name='customer-home'),
     path("register/", RegisterView.as_view(), name='register'),
     path("login/", LoginView.as_view(), name='login'),
     path("booking/", CustomerBooking.as_view(), name='customer-booking'),
+    path("booking/history", CustomerHistory.as_view(), name='customer-history'),
+    path('booking/history/cancel/<int:id>/', CustomerHistory.as_view(), name='cancel-booking'),
     path("manager/bookinglist/", BookingList.as_view(), name='bookinglist'),
     path("logout/", LogoutView.as_view(), name='logout'),
     path("manager/manageroom/", ManageRoom.as_view(), name='manage-room'),
